067e430cf602149817733ba0069512ef7942e040
rishabkhanna27 <rishabkhanna27@gmail.com>
2020-06-07 23:25:35 +0530

    Updates

---
+++ precache-manifest.3580940c13fd3869a4b20db987c4650f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c09e29b5d257744880e00a5a6c9fa4f",
+    "url": "/Fandom-react-project/index.html"
+  },
+  {
+    "revision": "30ff06434af8612e99ad",
+    "url": "/Fandom-react-project/static/css/main.a729fa36.chunk.css"
+  },
+  {
+    "revision": "82efe621e86c901c1493",
+    "url": "/Fandom-react-project/static/js/2.c12124ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Fandom-react-project/static/js/2.c12124ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30ff06434af8612e99ad",
+    "url": "/Fandom-react-project/static/js/main.91f87670.chunk.js"
+  },
+  {
+    "revision": "4323c1e2d0cbbb1ab787",
+    "url": "/Fandom-react-project/static/js/runtime-main.84b11c8a.js"
+  },
+  {
+    "revision": "1890127113ba7aae0536e3ab7d572fb4",
+    "url": "/Fandom-react-project/static/media/image.18901271.jpeg"
+  }
+]);
